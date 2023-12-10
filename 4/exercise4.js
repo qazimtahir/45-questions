6199e7b536ec8b16602ef11b2515216e3e882238
Muhammad Tahir Iqbal <qazimtahir@gmail.com>
2023-12-10 23:40:15 +0500

    Added exercise 4

---
+++ 4/exercise4.js
@@ -0,0 +1,9 @@
+console.log("\nFamous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:\n\nAlbert Einstein once said, \u201CA person who never made a mistake never tried anything new.\u201D\n");
+// SOLUTIONS
+console.log("Solution:\nPrinting using concatination\n");
+console.log("William Shakespeare once said in Hamlet, " +
+    '"' +
+    "There is nothing either good or bad, but thinking makes it so." +
+    '"');
+console.log("\nSolution:\nPrinting using string litterals\n");
+console.log("William Shakespeare once said in Hamlet, \"There is nothing either good or bad, but thinking makes it so.\"\n");
